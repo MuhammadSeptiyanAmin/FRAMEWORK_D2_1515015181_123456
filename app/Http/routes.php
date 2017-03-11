af73d8a74525b0de15284767ff151b44c8545a63
muhammadSeptiyan <muhammadseptiyan996@gmail.com>
2017-03-11 22:22:33 +0800

    tugas 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello-world', function () {
+    return 'hello World';
+});
+
+Route::get('pengguna/{septiyan}', function ($pengguna) {
+    return "Hallo world dari pengguna $pengguna";
+
+});
+
+Route::get('kelasd2/framework/{mhs?}', function ($mhs="Septiyan") {
+    return "Selamat datang $mhs";
+
+});
